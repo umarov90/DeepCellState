9ecbafd7b6ad472f1d939f7872d1ae4889dbe336
Umarov Ramzan <umarov90@hiroshima-u.ac.jp>
2020-05-24 15:48:31 +0900

    ver 1.02

--- data_parser_new.py
+++ data_parser_new.py
@@ -26,24 +26,28 @@
 import cmapPy.pandasGEXpress.parse_gctx as pg
 import cmapPy.pandasGEXpress.subset_gctoo as sg
 import pandas as pd
+import os
 
-gctx_file = "GSE70138_Broad_LINCS_Level5_COMPZ_n118050x12328_2017-03-06.gctx"
+data_folder = "/home/user/data/DeepFake/"
+os.chdir(data_folder)
 
-my_col_metadata = pg.parse(gctx_file, col_meta_only=True)
-sig_info = pd.read_csv("GSE70138_Broad_LINCS_sig_info.txt", sep="\t")
+gctx_file = "GSE92742_Broad_LINCS_Level5_COMPZ.MODZ_n473647x12328.gctx"
+
+sig_info = pd.read_csv("GSE92742_Broad_LINCS_sig_info.txt", sep="\t")
 gene_info = pd.read_csv("GSE92742_Broad_LINCS_gene_info.txt", sep="\t", dtype=str)
 landmark_gene_row_ids = gene_info["pr_gene_id"][gene_info["pr_is_lm"] == "1"]
+sig_info.set_index("sig_id", inplace=True)
 
-df = pg.parse(gctx_file, rid=landmark_gene_row_ids)
-print(df.data_df.shape)
+gctoo = pg.parse(gctx_file, cid=sig_info.index.tolist(), rid=landmark_gene_row_ids)
+gctoo.col_metadata_df = sig_info.copy()
 
 # vorinostat_sig_id_info =  sig_info[sig_info["pert_iname"] == "vorinostat"]
 # vorinostat_sig_id_info[0:5]
 # vorinostat_sig_id_info.set_index("sig_id", inplace=True)
 # vorinostat_sig_id_info.index
 
-sig_info.set_index("sig_id", inplace=True)
-df.col_metadata_df = sig_info
+#.set_index("sig_id", inplace=True)
+#df.col_metadata_df = sig_info
 
 # .value_counts()[:10].index.tolist()
 
@@ -56,21 +60,30 @@ df.col_metadata_df = sig_info
 #a375_pert_ids = df.col_metadata_df.index[df.col_metadata_df["cell_id"] == "A375"].tolist()
 #meta_data = df.col_metadata_df.tolist()
 
-df = df.data_df
+#vorinostat_sig_info = sig_info[(sig_info["pert_iname"] == "vorinostat") & (sig_info["pert_idose"] == "10 µM") & (sig_info["pert_itime"] == "24 h")]
+# set sig_id as index
+#vorinostat_sig_info.set_index("sig_id", inplace=True)
+#vorinostat_gctoo = sg.subset_gctoo(df, cid=vorinostat_sig_info.index.tolist())
+# annotate with its corresponding sample annotations
+#vorinostat_gctoo.col_metadata_df = vorinostat_sig_info.copy()
+
+df_data = gctoo.data_df
 #df2 = mcf7_gctoo.data_df
 
 #df1 = df1.drop('rid', 1)
 #df2 = df2.drop('rid', 1)
 
-df = df.transpose()
+df_data = df_data.transpose()
 #df2 = df2.transpose()
 
-df["cell_id"] = sig_info["cell_id"]
-df["pert_id"] = sig_info["pert_id"]
-df["pert_idose"] = sig_info["pert_idose"]
-df["pert_itime"] = sig_info["pert_itime"]
+df_data["cell_id"] = gctoo.col_metadata_df["cell_id"]
+df_data["pert_id"] = gctoo.col_metadata_df["pert_id"]
+df_data["pert_idose"] = gctoo.col_metadata_df["pert_idose"]
+df_data["pert_itime"] = gctoo.col_metadata_df["pert_itime"]
+df_data["distil_id"] = gctoo.col_metadata_df["distil_id"]
 
-df.to_csv("cell_data.tsv", sep='\t')
+
+df_data.to_csv("cell_data.tsv", sep='\t')
 
 # print("A375 " + str(a375_gctoo.data_df.shape))
 # print("MCF7 " + str(mcf7_gctoo.data_df.shape))
